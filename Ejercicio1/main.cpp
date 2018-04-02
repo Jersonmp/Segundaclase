877ba3a0126f60e0106106cfa5b67855bf0cd57a
JersonUCSP <jerson.mamani@ucsp.edu.pe>
2018-04-02 08:10:46 -0500

    Mayor o menor de edad

---
+++ Ejercicio1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Introduce tu edad ---> ";
+    int x;
+    cin>>x;
+    if (x>=18)
+        cout<<"Eres mayor de edad "<<endl;
+    else
+        cout<<"Eres menor de edad"<<endl;
+
+}
